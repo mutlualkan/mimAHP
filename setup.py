b7341eb06f5797f044429915d29de4f9be215fc7
Mutlu ALKAN <mutlualkan@gmail.com>
2025-10-01 08:52:33 +0300

    setup.py dosyasına packages ayarları eklendi.

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
 name='mimAHP',
 version='0.1',
-packages=find_packages(),
+packages=find_packages(where="src"),
+package_dir={"": "src"},
 description='Analitik Hiyerarşi Süreci (AHP) için Python kütüphanesi',
 author='Mutlu ALKAN',
 author_email='mutlualkan@gmail.com',classifiers=['Programming Language :: Python :: 3','License :: OSI Approved :: MIT License',],
